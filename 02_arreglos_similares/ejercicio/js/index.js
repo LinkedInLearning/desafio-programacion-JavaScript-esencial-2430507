e11ebe998785059a574234d6fd102a880db8eb02
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-14 01:37:38 +0000

    02 inicio

---
+++ 02_arreglos_similares/ejercicio/js/index.js
@@ -0,0 +1,10 @@
+
+// Determinar si ambos arreglos son similares:
+
+function sonSimilares(arregloA, arregloB) {
+
+}
+
+console.log(sonSimilares([1, 2], [1, 2, 3])); // false
+console.log(sonSimilares([1, 2, 3], [1, 2, 3])); // true
+console.log(sonSimilares([1, 2, 3], ['1','2','3'])); // false
