8bd058024ef6c87f440dc3d50e6d4b516b5aabf8
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-14 01:25:49 +0000

    ejercicio 01 inicio
    ejercicio 01 inicio

---
+++ 01_numero_primo/ejercicio/js/index.js
@@ -0,0 +1,15 @@
+
+// Escribir función que indica si un número es primo
+
+function esPrimo(numero) {
+
+    // escribe tu código aquí
+    
+
+}
+
+console.log(esPrimo(2)); //  true
+console.log(esPrimo(7)); //  true
+console.log(esPrimo(10)); // false
+console.log(esPrimo(15)); // false
+console.log(esPrimo(29)); // true
