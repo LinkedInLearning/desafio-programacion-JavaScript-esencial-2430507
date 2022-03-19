28fea3b6915b503fe95cdc797bf8eb53b4229d9c
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-19 21:05:38 +0000

    15_i

---
+++ 15_validacion_telefono/ejercicio/js/index.js
@@ -0,0 +1,12 @@
+
+// Validar que el número de teléfono tenga el formato correcto:
+// XXXX-XX-XX
+
+const input = document.getElementById('input');
+const buscar = document.getElementById('buscar');
+const resultado = document.getElementById('resultado');
+
+buscar.addEventListener('click', ()=> {
+    const valor = input.value;
+});
+
