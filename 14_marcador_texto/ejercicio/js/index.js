f87765930b8953dd8ae83fd9fec1b39097f238bc
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-19 21:03:19 +0000

    14_i

---
+++ 14_marcador_texto/ejercicio/js/index.js
@@ -0,0 +1,8 @@
+
+// Implementar el buscador de texto:
+// Al hacer click en el botón, se deberá buscar el texto que el usuario a
+// agregado al campo de texto y marcarlo en negrita en el párrafo.
+
+const texto = document.getElementById('texto');
+const input = document.getElementById('input');
+const buscar = document.getElementById('buscar');
