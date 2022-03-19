997286e2a161b8cf420ac30e15c80c0b65125c0e
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-19 20:48:39 +0000

    04_i

---
+++ 04_busqueda_productos/ejercicio/js/index.js
@@ -0,0 +1,12 @@
+
+//Crear array de productos que cada uno incluya información sobre id, nombreProducto, marca, fechaExpiracion:
+
+
+// Crear dos funciones para buscar productos acorde a los parámetros:
+
+function buscarProductosPorId(id) {
+}
+
+
+function buscarProductosPorFecha(fecha) {
+}
