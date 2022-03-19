b601a61c3687132a4ccf8ae8b9eeb956ad7c2912
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-19 20:56:51 +0000

    08_i

---
+++ 08_carrito_compras/ejercicio/js/index.js
@@ -0,0 +1,10 @@
+
+// 1. Escribir una clase para guardar el nombre, la fecha de expiracion 
+// y el precio de venta de los productos.
+
+
+
+// 2. Escribir una función que tome una lista de productos y sume el total del costo
+// de los productos, excuyendo los productos expirados.
+function carritoDeCompra(productos) {
+}
