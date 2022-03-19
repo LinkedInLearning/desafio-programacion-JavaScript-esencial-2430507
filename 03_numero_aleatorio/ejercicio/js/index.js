5b27cb48f6fe3f72d5f44582eef98b018c2791a6
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-19 20:47:19 +0000

    03_f

---
+++ 03_numero_aleatorio/ejercicio/js/index.js
@@ -0,0 +1,10 @@
+
+// Generar un número aleatorio dentro de un rando de dos números:
+
+function numeroAleatorio(numeroA, numeroB){
+}
+
+console.log(numeroAleatorio(5, 10));
+console.log(numeroAleatorio(-25, -22));
+console.log(numeroAleatorio(100, 0));
+console.log(numeroAleatorio(-2, 2));
