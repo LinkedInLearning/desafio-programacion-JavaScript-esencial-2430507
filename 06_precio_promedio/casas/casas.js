5305e10ff5a3e82a6883a68560218c1f25d90af4
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-19 20:51:04 +0000

    06_i

---
+++ 06_precio_promedio/casas/casas.js
@@ -0,0 +1,150 @@
+
+const casas = [
+    {
+        id: 1,
+        habitaciones: 4,
+        tamano: 200,
+        localidad: 'San José',
+        precio: 150000
+    },
+    {
+        id: 2,
+        habitaciones: 2,
+        tamano: 75,
+        localidad: 'San José',
+        precio: 80000
+    },
+    {
+        id: 3,
+        habitaciones: 3,
+        tamano: 100,
+        localidad: 'San José',
+        precio: 80000
+    },
+    {
+        id: 4,
+        habitaciones: 3,
+        tamano: 110,
+        localidad: 'Heredia',
+        precio: 85000
+    },
+    {
+        id: 5,
+        habitaciones: 3,
+        tamano: 110,
+        localidad: 'Heredia',
+        precio: 85000
+    },
+    {
+        id: 6,
+        habitaciones: 3,
+        tamano: 110,
+        localidad: 'Heredia',
+        precio: 85000
+    },
+    {
+        id: 7,
+        habitaciones: 2,
+        tamano: 60,
+        localidad: 'Cartago',
+        precio: 40000
+    },
+    {
+        id: 8,
+        habitaciones: 4,
+        tamano: 200,
+        localidad: 'Cartago',
+        precio: 140000
+    },
+    {
+        id: 9,
+        habitaciones: 2,
+        tamano: 60,
+        localidad: 'Cartago',
+        precio: 40000
+    },
+    {
+        id: 10,
+        habitaciones: 4,
+        tamano: 120,
+        localidad: 'Puntarenas',
+        precio: 80000
+    },
+    {
+        id: 11,
+        habitaciones: 3,
+        tamano: 130,
+        localidad: 'Puntarenas',
+        precio: 90000
+    },
+    {
+        id: 12,
+        habitaciones: 2,
+        tamano: 87,
+        localidad: 'Puntarenas',
+        precio: 60000
+    },
+    {
+        id: 13,
+        habitaciones: 3,
+        tamano: 95,
+        localidad: 'Guanacaste',
+        precio: 120000
+    },
+    {
+        id: 14,
+        habitaciones: 2,
+        tamano: 150,
+        localidad: 'Guanacaste',
+        precio: 130000
+    },
+    {
+        id: 15,
+        habitaciones: 1,
+        tamano: 45,
+        localidad: 'Guanacaste',
+        precio: 10000
+    },
+    {
+        id: 16,
+        habitaciones: 3,
+        tamano: 75,
+        localidad: 'Alajuela',
+        precio: 100000
+    },
+    {
+        id: 17,
+        habitaciones: 3,
+        tamano: 95,
+        localidad: 'Alajuela',
+        precio: 120000
+    },
+    {
+        id: 18,
+        habitaciones: 3,
+        tamano: 110,
+        localidad: 'Alajuela',
+        precio: 130000
+    },
+    {
+        id: 19,
+        habitaciones: 2,
+        tamano: 60,
+        localidad: 'Limón',
+        precio: 30000
+    },
+    {
+        id: 20,
+        habitaciones: 3,
+        tamano: 120,
+        localidad: 'Limón',
+        precio: 130000
+    },
+    {
+        id: 21,
+        habitaciones: 1,
+        tamano: 40,
+        localidad: 'Limón',
+        precio: 20000
+    }
+]
---
+++ 06_precio_promedio/ejercicio/js/index.js
@@ -0,0 +1,3 @@
+
+// Calcular el precio de las casas por localidad.
+// la constante casas contiene la información de casas.
